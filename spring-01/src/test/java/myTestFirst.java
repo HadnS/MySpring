113fe04fe0ea62512c7e5002b5df3672f68c79fb
HadnS <45629458+HadnS@users.noreply.github.com>
2020-09-27 14:47:46 +0800

    Update myTestFirst.java

--- spring-01/src/test/java/myTestFirst.java
+++ spring-01/src/test/java/myTestFirst.java
@@ -1,6 +1,9 @@
 import dao.UserMysqlImp;
 import service.UserServiceFirstImp;
+/*
+注释
 
+*/
 public class myTestFirst {
     public static void main(String[] args) {
         UserServiceFirstImp test  = new UserServiceFirstImp();
